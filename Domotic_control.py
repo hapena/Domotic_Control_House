61e9980b63145e396c9581417944d657fbad31c4
hapena <71275875+hapena@users.noreply.github.com>
2022-08-19 18:05:11 -0500

    Add files via upload

--- Domotic_control.py
+++ Domotic_control.py
@@ -25,7 +25,12 @@ lluvia = ADC(Pin(34))
 lluvia.width(ADC.WIDTH_10BIT) 
 lluvia.atten(ADC.ATTN_11DB)
 
+caudal = ADC(Pin(35))
+caudal.width(ADC.WIDTH_10BIT) 
+caudal.atten(ADC.ATTN_11DB)
+
 ventilador = Pin(19, Pin.OUT)
+
 bombillo  = Pin(5, Pin.OUT)
 
 puerta = PWM(Pin(12), freq=50)
@@ -57,50 +62,69 @@ def conectaWifi (red, password):
  
 #------------------------------------[BOT]---------------------------------------------------------------------#
 
-if conectaWifi ("INFORMATICA2", "Adminredp2017"):
+if conectaWifi ("Hugo", "Hupe6493"):
 
     print ("Conexión exitosa!")
     print('Datos de la red (IP/netmask/gw/DNS):', miRed.ifconfig())
     
+    url = "https://api.thingspeak.com/update?api_key=T22D86IN1PMTEF2Z" 
+    
     while True:
-            
+        
+        sensorDHT.measure()
+        temp = sensorDHT.temperature()
+        hum = sensorDHT.humidity()
+        lectura_gas = gas.read()
+        lectura_luz = luz.read()
+        lectura_lluvia = lluvia.read()
+        caudalimetro = caudal.read() 
+        
+        
+        r = urequests.get(url+"&field1="+str(temp)+
+                          "&field2="+str(hum)+
+                          "&field3="+str(lectura_gas)+
+                          "&field4="+str(lectura_luz)+
+                          "&field5="+str(lectura_lluvia)+
+                          "&field6="+str(caudalimetro))
+        print(r.status_code)
+        r.close ()
+        
+                  
         @bot.add_message_handler("Hola")
         def help(update):
-            update.reply('''¡Bienvenido!
+            update.reply('''¡Bienvenido! 
                          \n Menu Principal                                        
-                         \n Domotic Control House
+                         \n Domotic Control House U0001F916
                          \n Estado de tu Hogar...... 
                          \n Elije una opción:
                          
-                         Temperatura : Temp
-                         Gas: Gas
-                         Luz Dia: Luz
-                         Lluvia y Humedad: Clima
-                         Activar Aire A: Airea
-                         Deasactivar Aire A:Aired
-                         Encender Iluminacion : On
-                         apagar Iluminacion : Off 
-                         Abrir Puerta: Open
-                         Cerrar Puerta : Close
-                         Estado Puerta: Estado
-                                                
+                         Temperatura : Temp U0001F912
+                         Gas: Gas U0001F480
+                         Luz Dia: Luz U0001F60E
+                         Lluvia y Humedad: Clima U0001F976
+                         Consumo de Agua: Caudal U0001F62D
+                         Activar Aire A: Airea U0001F44D
+                         Deasactivar Aire A:Aired U0001F44E
+                         Encender Iluminacion : On  U0001F44D
+                         apagar Iluminacion : Off  U0001F44E
+                         Abrir Puerta: Open  U0001F44D
+                         Cerrar Puerta : Close U0001F44E
+                         Estado Puerta: Estado U0001F91A
+                         Ver DashBoard: Ver U0001F4BB
+                                                                                                
                          \n No olvides que estoy para tu seguridad y confort
-                         \n garcias''')
+                         \n garcias  U0001F600''')
               
         
         @bot.add_message_handler("Temp")
         def sensor_temperatura(update):
-            
-            sensorDHT.measure()
-            temp = sensorDHT.temperature()
-            hum = sensorDHT.humidity()       
+                              
             update.reply("Temperatura: "+ str(temp) + "°C  " + "Humedad: " + str(hum) + "%")
             
         
         @bot.add_message_handler("Gas")
         def sensor_gas(update):
-            
-            lectura_gas = gas.read()
+                        
             if lectura_gas > 600:
                 update.reply("Gas: "+ str(lectura_gas) + " Nivel Alto")
             else:
@@ -109,7 +133,6 @@ if conectaWifi ("INFORMATICA2", "Adminredp2017"):
         @bot.add_message_handler("Luz")
         def sensor_luz(update):
             
-            lectura_luz = luz.read()
             if lectura_luz < 600:
                 update.reply("Nivel Luminosidad: "+ str(lectura_luz) + " Dia")
             else:
@@ -117,13 +140,17 @@ if conectaWifi ("INFORMATICA2", "Adminredp2017"):
                 
         @bot.add_message_handler("Clima")
         def sensor_lluvia(update):
-            
-            lectura_lluvia = lluvia.read()
+                        
             if lectura_lluvia > 100:
                 update.reply("Clima: "+ str(lectura_lluvia) + " Clima lluviosos")
             else:
                 update.reply("Clima "+ str(lectura_lluvia) + " clima Seco")
-        
+                
+        @bot.add_message_handler("Caudal")
+        def sensor_caudal(update):
+                       
+            update.reply("El caudal es"+ str(caudalimetro))     
+             
         @bot.add_message_handler("Airea")
         def activar_aire(update):
             ventilador.off()
@@ -156,7 +183,6 @@ if conectaWifi ("INFORMATICA2", "Adminredp2017"):
             puerta.duty(m)
             update.reply("Puerta Cerrada")
             
-           
         @bot.add_message_handler("Estado")
         def estado_puerta(update):
                         
@@ -168,7 +194,12 @@ if conectaWifi ("INFORMATICA2", "Adminredp2017"):
             else:
                 update.reply("Estado de la puerta: 'Abierta' ")
                 
-                
+        @bot.add_message_handler("Ver")
+        def dashboard(update):
+                       
+            update.reply("https://thingspeak.com/channels/1807893") 
+         
+                      
         bot.start_loop()
     
 else:
